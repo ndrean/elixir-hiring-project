3dab950b98e41b4f114804985fb8a6b272b46c9b
Neven DREAN <nevendrean@yahoo.fr>
2023-10-31 18:21:10 +0100

    test

--- priv/repo/migrations/20231028005448_create_counter.exs
+++ priv/repo/migrations/20231028005448_create_counter.exs
@@ -2,7 +2,7 @@ defmodule Counter.Repo.Migrations.CreateCounter do
   use Ecto.Migration
 
   def change do
-    create_if_nots_exists table(:counters) do
+    create_if_not_exists table(:counters) do
       add :count, :integer
       add :region, :string
 
