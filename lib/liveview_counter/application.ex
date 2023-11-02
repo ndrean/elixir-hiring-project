518896d6d4b37734d3d91e96e70369b787a712c5
Neven DREAN <nevendrean@yahoo.fr>
2023-11-02 18:11:35 +0100

    rm dbg

--- lib/liveview_counter/application.ex
+++ lib/liveview_counter/application.ex
@@ -16,10 +16,10 @@ defmodule LiveviewCounter.Application do
     ]
 
     children = [
-      {Cluster.Supervisor, [topologies, [name: LiveViewCounter.ClusterSupervisor]]},
+      # {Cluster.Supervisor, [topologies, [name: LiveViewCounter.ClusterSupervisor]]},
       Counter.Repo,
       LiveviewCounterWeb.Telemetry,
-      # {DNSCluster, query: System.get_env("DNS_CLUSTER_QUERY") || :ignore},
+      {DNSCluster, query: System.get_env("DNS_CLUSTER_QUERY") || :ignore},
       {Phoenix.PubSub, name: LiveviewCounter.PubSub},
       LiveviewCounterWeb.Endpoint,
       LiveviewCounter.Presence
